e4f9de781a743dfcf670db54a1497f6d1e68add7
rainierrr <b1916320@planet.kanazawa-it.ac.jp>
2023-01-13 22:30:37 +0900

    update argo

--- locust/src/fog-client.py
+++ locust/src/fog-client.py
@@ -7,6 +7,7 @@ MG_NODE_B_IP = '54.255.41.147'
 SUPER_APP_PORT = 3000
 MG_APP_PORT = 30002
 FOG_APP_PORT = 30003
+CPU_THRESHOLD = 0.8
 
 
 def get_request(url):
@@ -39,25 +40,56 @@ def get_cluster_metrics(node_ip):
     return cluster_metrics
 
 
-def find_node():
-    mg_nodes_list = []
+def setup():
+    mg_node_list = []
     mg_nodes = get_mg_nodes_info()
+
     for mg_node in mg_nodes:
-        mg_node_info = {}
-        cluster_metrics = get_cluster_metrics(mg_node['ip'])
+        mg_node_dict = {}
+        mg_node_dict['name'] = mg_node['name']
+        mg_node_dict['ip'] = mg_node['ip']
 
-        mg_node_info['name'] = mg_node['name']
-        mg_node_info['ip'] = mg_node['ip']
-        mg_node_info['cpu'] = cluster_metrics['cpu']
-        mg_node_info['rtt'] = ping3.ping(mg_node['ip'])
+        mg_node_list.append(mg_node_dict)
 
-        mg_nodes_list.append(mg_node_info)
+    return mg_node_list
 
-    print(mg_nodes_list)
+
+def select_node(mg_node_list):
+    min_rtt = min([mg_node_dict['rtt']
+                  for mg_node_dict in mg_node_list])
+    print(f'min_rtt: {min_rtt}')
+    selected_node = [
+        mg_node_dict for mg_node_dict in mg_node_list if mg_node_dict['rtt'] == min_rtt][0]
+    print(f'selected_node: {selected_node}')
+    return selected_node
+
+
+def find_node(mg_node_list):
+    hight_cpu_node_list = []
+    low_cpu_node_list = []
+    for mg_node_dict in mg_node_list:
+        cluster_metrics = get_cluster_metrics(mg_node_dict['ip'])
+
+        mg_node_dict['cpu'] = cluster_metrics['cpu']
+        mg_node_dict['rtt'] = ping3.ping(mg_node_dict['ip'])
+
+        if cluster_metrics['cpu'] > CPU_THRESHOLD:
+            hight_cpu_node_list.append(mg_node_dict)
+        else:
+            low_cpu_node_list.append(mg_node_dict)
+
+    # CPUが閾値を超えていないノードがあれば、そのノードを選択
+    if len(low_cpu_node_list) > 0:
+        return select_node(low_cpu_node_list)
+
+    return select_node(hight_cpu_node_list)
 
 
 def main():
-    find_node()
+    mg_node_list = setup()
+    selected_node = find_node(mg_node_list)
+    print(f'selected node name: {selected_node["name"]}')
+    print(f'ip: {selected_node["ip"]}')
 
 
 if __name__ == '__main__':
