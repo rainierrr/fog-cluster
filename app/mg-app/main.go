a62425ce127a3db5264f9db2032e4358e8560087
rainierrr <b1916320@planet.kanazawa-it.ac.jp>
2023-01-15 20:05:51 +0900

    mg-appの返すClusterCPUをキャッシュ

--- app/mg-app/main.go
+++ app/mg-app/main.go
@@ -4,22 +4,22 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
-	// "github.com/gin-contrib/cache/persistence"
-	// "github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	//store := persistence.NewInMemoryStore(10 * time.Second)
+	store := persistence.NewInMemoryStore(3 * time.Second)
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"message": "OK"})
 	})
 
-	// router.GET("/cluster_metrics", cache.CachePage(store, 10*time.Second, getClusterUsageRateHandler))
-	router.GET("/cluster_metrics", getClusterUsageRateHandler)
+	router.GET("/cluster_metrics", cache.CachePage(store, 3*time.Second, getClusterUsageRateHandler))
 	router.GET("/cpu_usage_rate", getCPUUsageRateHandler)
 	port := os.Getenv("PORT")
 	if port == "" {
